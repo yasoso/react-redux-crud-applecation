993613017b0dccd38c9590a6184455400e445b19
Yaso <syasoda@mikilab.doshisha.ac.jp>
2020-04-18 16:57:55 +0900

    prpos-types

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profile = [
     {
-      name:"Taro",age:10
+      name:"Taro",age:20
     },
     {
       name: "Hanako",
       age: 5
     },
     {
-      name: "aya"
+      name: "ayako",
+      age:30
     }
   ]
   return <div>
@@ -36,8 +37,9 @@ const  User = (props) => {
   return <div > Hi I am {props.name},and {props.age} </div>
 }
 
-User.defaultProps = {
-    age:1
+User.propTypes = {
+  name: PropTypes.string,
+  age:  PropTypes.number.isRequired
 }
-  
+
 export default App;
