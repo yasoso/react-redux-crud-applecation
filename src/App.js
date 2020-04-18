6a2375b5fac528ba3d543b9cbe39a2e1ecc9c5e9
Yaso <syasoda@mikilab.doshisha.ac.jp>
2020-04-18 11:22:59 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 
 
-// class App extends Component{
-//   render() {
-//     return <div >Hello, World  </div>;
-//   }
-// }
-
 class App extends Component {
   render() {
-    return React.createElement(
-      "h1",
-      null,
-      "Hello,world!!"
-    );
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => { console.log("I am clicked") }}/>;
+      </React.Fragment>
+    )
   }
 }
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "h1",
+//       null,
+//       "Hello,world!!"
+//     );
+//   }
+// }
       
 export default App;
