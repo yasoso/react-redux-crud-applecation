67191c85465747efccc6d7f4b07922ff6de5671d
Yaso <syasoda@mikilab.doshisha.ac.jp>
2020-04-18 16:27:20 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => { console.log("I am clicked") }}/>;
-      </React.Fragment>
-    )
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => { console.log("I am clicked") }}/>;
+//       </React.Fragment>
+//     )
+//   }
+// }
+const App = () => {
+  return <div>
+    <Cat/>
+  </div>
 }
 
+const  Cat = () => {
+  return <div > Meaw </div>
+}
 // class App extends Component {
 //   render() {
 //     return React.createElement(
