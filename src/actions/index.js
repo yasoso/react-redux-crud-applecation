8bb78b3c3b8988b948ccaac367ea757c47d9eb37
Yaso <syasoda@mikilab.doshisha.ac.jp>
2020-04-20 17:34:17 +0900

    eventnew component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENTS = 'CREATE_EVENTS'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -14,3 +15,11 @@ export const readEvents = () => async dispatch => {
     })
 }
 
+export const postEvent = values => async dispatch => {
+    const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`,values)
+    console.log(response)
+    dispatch({
+        type: CREATE_EVENTS,
+        response
+    })
+}
