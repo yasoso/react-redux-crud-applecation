91f3144ea7d4acb28285024926bc597dc2764302
Yaso <syasoda@mikilab.doshisha.ac.jp>
2020-04-22 11:09:08 +0900

    update show

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,13 +3,15 @@ import axios from 'axios'
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENTS = 'CREATE_EVENTS'
 export const DELETE_EVENTS = 'DELETE_EVENTS'
+export const READ_EVENT = 'READ_EVENT'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
 
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
-    console.log(response)
+    
     dispatch({
         type: READ_EVENTS,
         response
@@ -23,6 +25,21 @@ export const postEvent = values => async dispatch => {
         type: CREATE_EVENTS,
         response
     })
+}
+
+export const putEvent = values => async dispatch => {
+    
+    const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+    
+    dispatch({
+        type: UPDATE_EVENT,
+        response
+    })
+}
+
+export const getEvent = id => async dispatch => {
+    const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    dispatch({type:READ_EVENT,response})
 }
 
 export const deleteEvent = id => async dispatch => {
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,20 @@
-import { READ_EVENTS,DELETE_EVENTS } from '../actions'
+import { CREATE_EVENTS,READ_EVENTS,DELETE_EVENTS,READ_EVENT,UPDATE_EVENT} from '../actions'
 // json をidごとに分割
 import _  from 'lodash'
 
 export default (events = {}, action) => {
     switch (action.type) {
+        case CREATE_EVENTS:
+        case UPDATE_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
+            //{id: 5, title: "Let's have an event 5!", body: "This is the body for event 5."}
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
         case DELETE_EVENTS:
             delete events[action.id]
-            return {...events}
+            return { ...events }
+            
         default:
             return events
     }
